b9553af091ef8bf28bd3c46c7b218f0e427d9aa9
TAIGA\km101 <km10150301@gmail.com>
2024-11-20 17:34:18 +0900

    スタイル調整

--- frontend/src/components/AddTodoForm/AddTodoForm.jsx
+++ frontend/src/components/AddTodoForm/AddTodoForm.jsx
@@ -5,18 +5,19 @@ import { AuthContext } from "../../context/AuthContext";
 import SubmitButton from "../SubmitButton/SubmitButton";
 
 const AddTodoForm = () => {
-  const [error, setError] = useState();
+  const { state } = useContext(AuthContext);
+  const [error, setError] = useState(state.error);
   const [priority, setPriority] = useState();
   const title = useRef();
   const content = useRef();
-  const { state } = useContext(AuthContext);
+
   const user = state.user;
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await apiClinet.post("http://localhost:5000/api/todo/", {
+      const res = await apiClinet.post("/todo", {
         userId: user._id,
         title: title.current.value,
         content: content.current.value,
@@ -53,60 +54,73 @@ const AddTodoForm = () => {
           <label htmlFor="content" className={styles.form__label}>
             コンテンツ
           </label>
-          <input
+          <textarea
             type="text"
             id="content"
             ref={content}
             required
-            className={styles.form__inputText}
+            className={styles.form__textArea}
           />
         </div>
-        <div className={styles.form__item}>
-          <input
-            type="radio"
-            name="priority"
-            value="高"
-            onChange={(e) => setPriority(e.target.value)}
-            id="high"
-            checked={priority === "高"}
-            required
-          />
-          <label htmlFor="high" className={styles.form__label}>
-            高
-          </label>
-          <input
-            type="radio"
-            name="priority"
-            value="中"
-            onChange={(e) => setPriority(e.target.value)}
-            id="middle"
-            checked={priority === "中"}
-            required
-          />
-          <label htmlFor="middle" className={styles.form__label}>
-            中
-          </label>
-          <input
-            type="radio"
-            name="priority"
-            value="低"
-            onChange={(e) => setPriority(e.target.value)}
-            id="low"
-            checked={priority === "低"}
-            required
-          />
-          <label
-            htmlFor="low"
-            className={`${styles.form__label} ${styles["form__label--radio"]}`}
-          >
-            低
-          </label>
+        <div className={`${styles.form__item} ${styles["form__item--radio"]}`}>
+          <span>優先度</span>
+          <div>
+            <label
+              htmlFor="high"
+              className={`${styles.form__label} ${styles["form__label--radio"]}`}
+            >
+              高
+            </label>
+            <input
+              type="radio"
+              name="priority"
+              value="高"
+              onChange={(e) => setPriority(e.target.value)}
+              id="high"
+              checked={priority === "高"}
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="middle"
+              className={`${styles.form__label} ${styles["form__label--radio"]}`}
+            >
+              中
+            </label>
+            <input
+              type="radio"
+              name="priority"
+              value="中"
+              onChange={(e) => setPriority(e.target.value)}
+              id="middle"
+              checked={priority === "中"}
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="low"
+              className={`${styles.form__label} ${styles["form__label--radio"]}`}
+            >
+              低
+            </label>
+            <input
+              type="radio"
+              name="priority"
+              value="低"
+              onChange={(e) => setPriority(e.target.value)}
+              id="low"
+              checked={priority === "低"}
+              required
+            />
+          </div>
         </div>
-        <div>
+        <div className={styles.buttonWrapper}>
           <SubmitButton>追加</SubmitButton>
         </div>
       </form>
-      <div>{error}</div>
+      <div>{error && error}</div>
     </div>
   );
 };
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -2,22 +2,29 @@ import { useContext } from "react";
 import styles from "./Home.module.css";
 import { AuthContext } from "../../context/AuthContext";
 import TodoList from "../TodoList/TodoList";
-import AddTodo from "../AddTodoForm/AddTodoForm";
+import AddTodoForm from "../AddTodoForm/AddTodoForm";
+import Sheet from "../Sheet/Sheet";
+import CompletedTodo from "../CompletedTodo/CompletedTodo";
 
 const Home = () => {
   const { state } = useContext(AuthContext);
   const user = state.user;
 
   return (
-    <div className={styles.container}>
-      <p>
-        {user.username}さん、こんにちは！
-        <br />
-        あなたのタスクを登録しましょう！
-      </p>
-      <AddTodo />
-      <TodoList />
-    </div>
+    <Sheet>
+      <div className={styles.container}>
+        <p>
+          <b>{user.username}</b>さん、こんにちは！
+          <br />
+          あなたのタスクを登録しましょう！
+        </p>
+        <AddTodoForm />
+        <div className={styles.main}>
+          <TodoList />
+          <CompletedTodo />
+        </div>
+      </div>
+    </Sheet>
   );
 };
 
